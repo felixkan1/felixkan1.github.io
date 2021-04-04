185d720c327003ae57e2845217c1ec22aea90039
felixkan1 <felix.kan@mail.utoronto.ca>
2021-04-03 20:06:53 -0400

    Project done

---
+++ js/data/songs.js
@@ -0,0 +1,85 @@
+export const songsList = [{
+    "id": "1",
+    "url": "https://www.dropbox.com/s/ktprv7c6kz6jxdf/acura%20integurlfrank%20ocean%20lyrics.mp3?dl=1",
+    "time": "01:58",
+    "title": "Acura Integurl",
+    "artist": "Frank Ocean"
+  },
+  {
+    "id": "2",
+    "url": "https://www.dropbox.com/s/g26zj8sjqdjkiag/Keri%20Hilsonft.%20Kanye%20West%2C%20Ne-Yo%20-%20Knock%20You%20Down%20%28Official%20Video%29.mp3?dl=1",
+    "time": "04:45",
+    "title": "Knock You Down",
+    "artist": "Keri Hilson ft. Kanye West, Ne-Yo"
+  },
+  {
+    "id": "3",
+    "url": "https://www.dropbox.com/s/jkk00zmamuti51u/Love%20Yourz.mp3?dl=1",
+    "time": "03:31",
+    "title": "Love Yourz",
+    "artist": "J. Cole"
+  },
+  {
+    "id": "4",
+    "url": "https://www.dropbox.com/s/ln13cbd35u6pg3y/January%2028th.mp3?dl=1",
+    "time": "04:29",
+    "title": "January 28th",
+    "artist": "J. Cole"
+  },
+  {
+    "id": "6",
+    "url": "https://www.dropbox.com/s/g7vp8oxmsyma8ck/My%20Love%20The%20Dream%20Ft.%20Mariah%20Carey%20w%20Lyrics.mp3?dl=1",
+    "time": "03:26",
+    "title": "My Love",
+    "artist": "Mariah Carey Ft. The Dream"
+  },
+  {
+    "id": "6",
+    "url": "../../music/shape-of-my-heart.mp3",
+    "time": "04:43",
+    "title": "Shape Of My Heart",
+    "artist": "Sting"
+  },
+  {
+    "id": "7",
+    "url": "../../music/seven-days.mp3",
+    "time": "03:57",
+    "title": "7 days",
+    "artist": "craig David"
+  },
+  {
+    "id": "8",
+    "url": "../../music/dilemma.mp3",
+    "time": "04:42",
+    "title": "Nelly Dilemma ft. keylly Rowland",
+    "artist": "Nelly"
+  },
+  {
+    "id": "9",
+    "url": "../../music/psycho.mp3",
+    "time": "03:57",
+    "title": "Post Malone Psycho Ft. Ty Dolla",
+    "artist": "Post malone"
+  },
+  {
+    "id": "10",
+    "url": "../../music/congratulations.mp3",
+    "time": "03:40",
+    "title": "Congratulations ft. Quavo",
+    "artist": "Post Malone"
+  },
+  {
+    "id": "11",
+    "url": "../../music/rockstar.mp3",
+    "time": "04:01",
+    "title": "Rockstar",
+    "artist": "Post Malone"
+  },
+  {
+    "id": "12",
+    "url": "../../music/sad.mp3",
+    "time": "02:46",
+    "title": "Sad",
+    "artist": "XXXtentacion"
+  },
+];
